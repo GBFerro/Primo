728c8b264783cd3ad050e2c15e3c5c4f7cb5ee79
GBFerro <giovani.ferro@usp.br>
2023-03-05 18:37:26 -0300

    Add project files.

---
+++ Primo/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double n;
+        int count = 0, primo = 0;
+
+        Console.WriteLine("Digite um número: ");
+        n = double.Parse(Console.ReadLine());
+
+        count = int.Parse(n.ToString());
+
+        while (count > 0)
+        {
+            if (n % count == 0)
+            {
+                primo++;
+            }
+
+            count--;
+        }
+            if (primo == 2) {
+                Console.WriteLine("O número é primo");
+            } else { Console.WriteLine("O número não é primo"); }
+
+    }
+}
